95df6697a449f1987e92c686774eec803a5d4d2e
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-19 14:36:16 +0200

    + Added file index.html / file js

---
+++ js/es-1.js
@@ -0,0 +1,6 @@
+/* Creare un oggetto che descriva uno studente,
+con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in
+tutte le proprietà dell'oggetto.*/
+
+
