90627ca2fb592358317a84d379eb9530d9e663ec
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-19 15:14:41 +0200

    + Es-1: Added ciclo for-in and innerHTML

--- js/es-1.js
+++ js/es-1.js
@@ -1,19 +1,11 @@
-/* Creare un oggetto che descriva uno studente,
-con le seguenti proprietà: nome, cognome e età.
-Stampare a schermo (in pagina) attraverso un ciclo for-in
-tutte le proprietà dell'oggetto.*/
-
-//? #1: Creare un nuovo oggetto "studente"
-//? #2: Aggiungere tre proprietà: nome / cognome / età
-//? #3: Collegare tramite una variabile l'id di un elemento in html
-//? #4: Creare un ciclo for-in 
-//? #5: Far stampare all'interno del ciclo tutte le proprietà dell'oggetto in pagina.
-
 /* -------------------------------------------------------------------------- */
 /*                                 Exercise 1                                 */
 /* -------------------------------------------------------------------------- */
+//* Link a variable to an html element and set variable to contain list items
+var displayElement = document.getElementById("display");
+var listItems = "";
 
-//* #1: Create a "student" object and enter three properties: firstName, lastName, age:
+//* Create a "student" object and enter three properties: firstName, lastName, age:
 var student = {
     firstName: "Mario",
     lastName: "Rossi",
@@ -21,7 +13,9 @@ var student = {
 };
 console.log(student);
 
-//* Link a variable to an html element:
-var displayElement = document.getElementById("display");
-
+//* Create an "for-in" loop to print all object properties:
+for (var property in student) {
+    listItems += "<li>" + student[property] + "</li>";
+}
+displayElement.innerHTML = listItems;
 
