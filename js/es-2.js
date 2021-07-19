dd0b3e5b870e597ddc82a8d78415c48b86ee2a94
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-19 16:17:15 +0200

    + Es-2: Added new student chosen by the user

--- js/es-2.js
+++ js/es-2.js
@@ -42,4 +42,31 @@ for (var i = 0; i < studentsList.length; i++) {
     console.log("LastName: " + currentElement.lastName);
 }
 
+//* Ask the user to enter a new student object with first name, last name and age:
+// Ask for student first name
+var userFirstName = prompt("Inserisci il nome dello studente");
+console.log("User firstName: ", userFirstName);
+
+// Ask for student last name
+var userLastName = prompt("Inserisci il cognome dello studente");
+console.log("User lastName: ", userLastName);
+
+// Ask for student age
+var userAge = prompt("Inserisci l'età dello studente");
+console.log("User age: ", userAge);
+
+
+//* Create a new student object with the data entered by the user:
+var newStudent = {
+    firstName: userFirstName,
+    lastName: userLastName,
+    age: userAge
+};
+
+console.log(newStudent);
+
+//* Add the new student to the student array:
+studentsList.push(newStudent);
+console.log(studentsList);
+
 
