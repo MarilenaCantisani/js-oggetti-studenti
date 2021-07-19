934830578f504de742bc6b36cff41232563dde0f
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-19 17:45:45 +0200

    + Es-2: Added print in html and validation user prompt

--- js/es-2.js
+++ js/es-2.js
@@ -2,6 +2,10 @@
 /*                                 Exercise 2                                 */
 /* -------------------------------------------------------------------------- */
 
+//* Link a variable to an html element and set variable to contain list items
+var displayElement = document.getElementById("display");
+var listItems = "";
+
 //* Create an array containing student objects: 
 var studentsList = [
     {
@@ -28,21 +32,35 @@ for (var i = 0; i < studentsList.length; i++) {
 
     //Print student surname:
     console.log("LastName: " + currentElement.lastName);
+
+    // Print in html first name and last name:
+    listItems += "<li>" + "Nome e cognome: " + currentElement.firstName + " " + currentElement.lastName + "</li>";
 }
+displayElement.innerHTML = listItems;
+
 
 
 //* Ask the user to enter a new student object with first name, last name and age:
 // Ask for student first name
-var userFirstName = prompt("Inserisci il nome dello studente");
-console.log("User firstName: ", userFirstName);
+do {
+    var userFirstName = prompt("Inserisci il nome dello studente");
+    console.log("User firstName: ", userFirstName);
+} while (!userFirstName || userFirstName.trim() === " " || !isNaN(userFirstName));
+
 
 // Ask for student last name
-var userLastName = prompt("Inserisci il cognome dello studente");
-console.log("User lastName: ", userLastName);
+do {
+    var userLastName = prompt("Inserisci il cognome dello studente");
+    console.log("User lastName: ", userLastName);
+} while (!userLastName || userLastName.trim() === " " || !isNaN(userLastName));
+
 
 // Ask for student age
-var userAge = prompt("Inserisci l'età dello studente");
-console.log("User age: ", userAge);
+do {
+    var userAge = prompt("Inserisci l'età dello studente");
+    console.log("User age: ", userAge);
+} while (!userAge || isNaN(userAge));
+
 
 
 //* Create a new student object with the data entered by the user:
@@ -56,7 +74,13 @@ console.log(newStudent);
 
 //* Add the new student to the student array:
 studentsList.push(newStudent);
+console.log("Dati studente: ", newStudent);
 console.log(studentsList);
+
+//* Print in html first name, last name and age:
+listItems += "<li>" + "Nome e cognome: " + newStudent.firstName + " " + newStudent.lastName + "; Age: " + newStudent.age + "</li>";
+displayElement.innerHTML = listItems;
+
 
 
 
